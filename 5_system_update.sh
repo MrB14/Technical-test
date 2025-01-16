f300aed258d0865356055659c19b8d3bf1f7c399
MrB14 <132985702+MrB14@users.noreply.github.com>
2025-01-17 00:34:31 +0700

    5_system_update.sh

--- 5_system_update.sh
+++ 5_system_update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 log_file="/var/log/system_update.log"
 if [ -x "$(command -v apt)" ]; then
-    sudo apt update && sudo apt upgrade -y > "$log_file"
+    sudo apt-get update && sudo apt-get upgrade -y > "$log_file"
 elif [ -x "$(command -v yum)" ]; then
     sudo yum update -y > "$log_file"
 else
